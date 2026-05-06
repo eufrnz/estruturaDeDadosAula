b9bf5a56d953ecefae12c3364ccf89b3a0a42825
eufrnz <felipeacfornazeiro@gmail.com>
2026-05-06 15:42:42 -0300

    sim

--- py3.py
+++ py3.py
@@ -1,2 +1,19 @@
 def somaVetor(v, n):
+   if(n == 0):
+       return v[n]
+   else: 
+       return v[n] + somaVetor(v, n - 1)
+
+def main():
+    n = input()
+    print("Entre com os numeros")
     v = []
+    for i in range(n):
+        numero = int(input(f"Numero (i + 1): "))
+        v.append(numero)
+    
+    resultado = somaVetor(v, n - 1)
+    print(f"Resultado = {resultado}")
+    
+if __name__ == "__main__":
+    main()
